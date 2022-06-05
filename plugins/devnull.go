7cde9098dd494c5712dc4f0c5ce4721bfe827c4a
Mikhail Knyazhev <markus621@gmail.com>
2022-06-06 02:06:59 +0300

    fix null logger + bump vendors

--- plugins/devnull.go
+++ plugins/devnull.go
@@ -17,11 +17,13 @@ func (devNullMetric) ExecutionTime(_ string, call func()) { call() }
 
 type devNullLogger struct{}
 
-func (devNullLogger) SetOutput(io.Writer)           {}
-func (devNullLogger) Fatalf(string, ...interface{}) {}
-func (devNullLogger) Errorf(string, ...interface{}) {}
-func (devNullLogger) Warnf(string, ...interface{})  {}
-func (devNullLogger) Infof(string, ...interface{})  {}
-func (devNullLogger) Debugf(string, ...interface{}) {}
-func (devNullLogger) SetLevel(v uint32)             {}
-func (devNullLogger) Close()                        {}
+func (devNullLogger) SetOutput(io.Writer)                           {}
+func (devNullLogger) Fatalf(string, ...interface{})                 {}
+func (devNullLogger) Errorf(string, ...interface{})                 {}
+func (devNullLogger) Warnf(string, ...interface{})                  {}
+func (devNullLogger) Infof(string, ...interface{})                  {}
+func (devNullLogger) Debugf(string, ...interface{})                 {}
+func (devNullLogger) SetLevel(v uint32)                             {}
+func (devNullLogger) Close()                                        {}
+func (devNullLogger) GetLevel() uint32                              { return 0 }
+func (v devNullLogger) WithFields(_ logger.Fields) logger.LogWriter { return v }
--- schema/common.go
+++ schema/common.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"time"
 
-	"github.com/pkg/errors"
+	"github.com/deweppro/go-errors"
 )
 
 //nolint: golint
